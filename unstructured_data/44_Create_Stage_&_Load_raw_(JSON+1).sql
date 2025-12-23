259c11f6855be3dcc5d48e5fdcec15d2c8b4c765
sanjaysbiswas <46516132+sanjaysbiswas@users.noreply.github.com>
2025-12-23 20:32:20 +0530

    Update 44_Create_Stage_&_Load_raw_(JSON+1).sql

--- unstructured_data/44_Create_Stage_&_Load_raw_(JSON+1).sql
+++ unstructured_data/44_Create_Stage_&_Load_raw_(JSON+1).sql
@@ -16,4 +16,55 @@ COPY INTO OUR_FIRST_DB.PUBLIC.JSON_RAW
     files = ('HR_data.json');
     
    
+
 SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+// First step: Load Raw JSON
+
+CREATE OR REPLACE stage MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+     url='s3://bucketsnowflake-jsondemo';
+     
+LIST @MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE;
+
+CREATE OR REPLACE file format MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    TYPE = JSON;
+    
+    
+CREATE OR REPLACE table OUR_FIRST_DB.PUBLIC.JSON_RAW (
+    raw_file variant);
+    
+COPY INTO OUR_FIRST_DB.PUBLIC.JSON_RAW
+    FROM @MANAGE_DB.EXTERNAL_STAGES.JSONSTAGE
+    file_format= MANAGE_DB.FILE_FORMATS.JSONFORMAT
+    files = ('HR_data.json');
+    
+   
+SELECT * FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+// Second step: Parse & Analyse Raw JSON 
+
+   // Selecting attribute/column
+
+SELECT RAW_FILE:city FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT $1:first_name FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+   // Selecting attribute/column - formattted
+
+SELECT RAW_FILE:first_name::string as first_name  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT RAW_FILE:id::int as id  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+SELECT 
+    RAW_FILE:id::int as id,  
+    RAW_FILE:first_name::STRING as first_name,
+    RAW_FILE:last_name::STRING as last_name,
+    RAW_FILE:gender::STRING as gender
+
+FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+   // Handling nested data
+   
+SELECT RAW_FILE:job as job  FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
