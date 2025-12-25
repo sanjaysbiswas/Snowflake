69e9d6fa8a2468a6c7c8372220d0d0203f285cad
sanjaysbiswas <46516132+sanjaysbiswas@users.noreply.github.com>
2025-12-25 10:57:53 +0530

    Add files via upload

--- unstructured_data/47_Dealing_with_hierarchy_(JSON_4).sql
+++ unstructured_data/47_Dealing_with_hierarchy_(JSON_4).sql
@@ -1,3 +1,6 @@
+
+
+
 SELECT 
     RAW_FILE:spoken_languages as spoken_languages
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
@@ -15,20 +18,27 @@ SELECT
      array_size(RAW_FILE:spoken_languages) as spoken_languages
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
 
+
+
 SELECT 
     RAW_FILE:spoken_languages[0] as First_language
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
 
 SELECT 
     RAW_FILE:first_name::STRING as first_name,
     RAW_FILE:spoken_languages[0] as First_language
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
 
+
 SELECT 
     RAW_FILE:first_name::STRING as First_name,
     RAW_FILE:spoken_languages[0].language::STRING as First_language,
     RAW_FILE:spoken_languages[0].level::STRING as Level_spoken
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW;
+
+
+
 
 SELECT 
     RAW_FILE:id::int as id,
@@ -52,34 +62,11 @@ SELECT
 FROM OUR_FIRST_DB.PUBLIC.JSON_RAW
 ORDER BY ID;
 
+
+
+
 select
       RAW_FILE:first_name::STRING as First_name,
     f.value:language::STRING as First_language,
    f.value:level::STRING as Level_spoken
 from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
-
-
-// Option 1: CREATE TABLE AS
-
-CREATE OR REPLACE TABLE Languages AS
-select
-      RAW_FILE:first_name::STRING as First_name,
-    f.value:language::STRING as First_language,
-   f.value:level::STRING as Level_spoken
-from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
-
-SELECT * FROM Languages;
-
-truncate table languages;
-
-// Option 2: INSERT INTO
-
-INSERT INTO Languages
-select
-      RAW_FILE:first_name::STRING as First_name,
-    f.value:language::STRING as First_language,
-   f.value:level::STRING as Level_spoken
-from OUR_FIRST_DB.PUBLIC.JSON_RAW, table(flatten(RAW_FILE:spoken_languages)) f;
-
-
-SELECT * FROM Languages;
